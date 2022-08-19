dc3b1d6eccd889d4d1c5372ac8a5a75d55561766
HusnaTohhad <6310110611@psu.ac.th>
2022-08-19 22:41:18 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import './App.css'
+import CharacterCard from './CharacterCard';
 
-import './App.css';
-
+const word = "Hello";
 function App() {
   return (
     <div>
-        Hello World
+      {
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
     </div>
   );
 }
